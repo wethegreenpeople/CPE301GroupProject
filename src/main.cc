8c6e3a4b8f2c75a28c9ffcb79a98b36942c918be
NoahGIsMe <noah@lordcalvert.net>
2021-11-21 20:56:27 -0800

    wrote a skeleton code for 'main' as well as the temperature sensor code

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,35 @@
+#include "DHT.h"
+#define DHTPIN 2 // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11 // DHT 11
+
 void main
 {
+    DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor
+
+    double tempReading = TemperatureSensor(); // get value from temperature sensor
+    double waterReading = WaterLevelSensor(); // 
+    double waterLevel = ; // need to decide what the minimum water level needs to be
+    double threshold = ; // need to decide what the minimum temperature (in Celcius per the library function) needs to be
     
+    while ((threshold <= tempReading) && (waterLevel <= waterReading)) // See if it's hot enough to run the fan and that the water level is high enough
+    {
+        runMotor();
+    }
+}
+
+double TemperatureSensor()
+{
+    delay(2000);
+    double temperature = dht.readTemperature(); // Read temperature
+    return temperature; // Return temperature reading
+}
+
+double WaterLevelSensor()
+{
+    return waterLevel;
+}
+
+double runMotor()
+{
+
 }
